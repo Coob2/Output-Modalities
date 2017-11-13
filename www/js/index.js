822ea6acd01fc49a9c79c228d611d92927c8dd65
Coob2 <coob2_17@uni.worc.ac.uk>
2017-11-13 12:00:46 +0000

    Added vibration and beeps

--- www/js/index.js
+++ www/js/index.js
@@ -12,15 +12,15 @@ $(document).on("pagecreate","#pageone",function(){
 
 function Beep()
 {
-  //navigator.notification.beep(1);
+  navigator.notification.beep(1);
   console.log("beep");
-  var text = !Math.round(Math.random());
-	console.log(text);
+  //var text = !Math.round(Math.random());
+	//console.log(text);
 }
 
 function Vibrate()
 {
-  //navigator.vibrate(1000);
+  navigator.vibrate(1000);
   console.log("vibrate");
 }
 
