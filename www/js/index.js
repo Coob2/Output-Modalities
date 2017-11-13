75d67e632b8660897de77b0de298e0ab778fefc7
Coob2 <coob2_17@uni.worc.ac.uk>
2017-11-13 11:32:01 +0000

    changed phonegapID to Modalities

--- www/js/index.js
+++ www/js/index.js
@@ -12,18 +12,16 @@ $(document).on("pagecreate","#pageone",function(){
 
 function Beep()
 {
-  navigator.notification.beep(1);
-  alert("Hello!");
+  //navigator.notification.beep(1);
+  console.log("beep");
+  var text = !Math.round(Math.random());
+	console.log(text);
 }
 
 function Vibrate()
 {
-  navigator.vibrate(1000);
-  alert("Hello");
-}
-
-function random() {
-	return !Math.round(Math.random());
+  //navigator.vibrate(1000);
+  console.log("vibrate");
 }
 
 function Nothing()
